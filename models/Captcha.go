feafd8521513b12ca81bc8927e1e9d6e2b0dd197
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:17:10 +0800

    验证码模型

---
+++ models/Captcha.go
@@ -0,0 +1,17 @@
+// 验证码模型
+package models
+
+import (
+	"github.com/astaxie/beego/cache"
+	"github.com/astaxie/beego/utils/captcha"
+)
+
+var Cpt *captcha.Captcha
+
+func init() {
+	store := cache.NewMemoryCache()
+	Cpt = captcha.NewWithFilter("/captcha/", store)
+	Cpt.ChallengeNums = 4
+	Cpt.StdWidth = 100
+	Cpt.StdHeight = 40
+}
