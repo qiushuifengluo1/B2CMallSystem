1651e7097fff150820ed19840795258ab0af0b31
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:16:10 +0800

    焦点图模型

---
+++ models/Banner.go
@@ -0,0 +1,21 @@
+// 焦点图模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type Banner struct {
+	Id         int
+	Title      string
+	BannerType int
+	BannerImg  string
+	Link       string
+	Sort       int
+	Status     int
+	AddTime    int
+}
+
+func (Banner) TableName() string {
+	return "banner"
+}
