bc490faa28b02e1867c46522848eeaee42d03f03
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:26:01 +0800

    商品类型模型

---
+++ models/ProductType.go
@@ -0,0 +1,18 @@
+// 商品类型模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type ProductType struct {
+	Id          int
+	Title       string
+	Description string
+	Status      int
+	AddTime     int
+}
+
+func (ProductType) TableName() string {
+	return "product_type"
+}
