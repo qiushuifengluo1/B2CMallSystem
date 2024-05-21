a8f9cb3b788148185610dda55392a6e04c826bd6
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:27:44 +0800

    后台角色模型

---
+++ models/Role.go
@@ -0,0 +1,18 @@
+// 后台角色模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type Role struct {
+	Id          int
+	Title       string
+	Description string
+	Status      int
+	AddTime     int
+}
+
+func (Role) TableName() string {
+	return "role"
+}
