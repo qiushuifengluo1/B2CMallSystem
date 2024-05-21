59713319b50f39b4e37753ab99154096a8157ef6
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:15:15 +0800

    后台管理员模型

---
+++ models/Administrator.go
@@ -0,0 +1,23 @@
+// 后台管理员模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type Administrator struct {
+	Id       int
+	Username string
+	Password string
+	Mobile   string
+	Email    string
+	Status   int
+	RoleId   int `gorm:"roleid"`
+	AddTime  int
+	IsSuper  int
+	Role     Role `gorm:"foreignkey:Id;association_foreignkey:RoleId"`
+}
+
+func (Administrator) TableName() string {
+	return "administrator"
+}
