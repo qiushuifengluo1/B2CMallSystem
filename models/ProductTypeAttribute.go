c1184d95d709e3db97bfb94df16bfe17f0cf4631
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:26:25 +0800

    商品类型属性模型

---
+++ models/ProductTypeAttribute.go
@@ -0,0 +1,21 @@
+// 商品类型属性模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type ProductTypeAttribute struct {
+	Id        int    `json:"id"`
+	CateId    int    `json:"cate_id"`
+	Title     string `json:"title"`
+	AttrType  int    `json:"attr_type"`
+	AttrValue string `json:"attr_value"`
+	Status    int    `json:"status"`
+	Sort      int    `json:"sort"`
+	AddTime   int    `json:"add_time"`
+}
+
+func (ProductTypeAttribute) TableName() string {
+	return "product_type_attribute"
+}
