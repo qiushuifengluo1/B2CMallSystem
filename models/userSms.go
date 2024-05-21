dc81967357bd557ee61b435f80f6cafe27d091bf
qsfl <qiushuifengluo@gmail.com>
2024-05-21 17:14:18 +0800

    用户短信验证模型

---
+++ models/userSms.go
@@ -0,0 +1,20 @@
+// 用户短信验证模型
+package models
+
+import (
+	_ "github.com/jinzhu/gorm"
+)
+
+type UserSms struct {
+	Id        int
+	Ip        string
+	Phone     string
+	SendCount int
+	AddDay    string
+	AddTime   int
+	Sign      string
+}
+
+func (UserSms) TableName() string {
+	return "user_sms"
+}
